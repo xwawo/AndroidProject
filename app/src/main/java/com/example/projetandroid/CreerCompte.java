f322a0bc1f44b0ccf563e18d18acad4b99c7d025
robsont <thomas.robson@etu.univ-grenoble-alpes.fr>
2021-03-31 14:54:43 +0200

    millieu programation fonction enregistrement de nouveau users

--- app/src/main/java/com/example/projetandroid/CreerCompte.java
+++ app/src/main/java/com/example/projetandroid/CreerCompte.java
@@ -2,10 +2,23 @@ package com.example.projetandroid;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import db.DatabaseClient;
+import db.User;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreerCompte extends AppCompatActivity {
+
+    // DATA
+    private DatabaseClient mDb;
+
+    // VIEW
+    private EditText editTextNomView;
+    private EditText editTextPrenomView;
+    private Button button_saveView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +26,15 @@ public class CreerCompte extends AppCompatActivity {
 
         // On charge le XML pour créer l'arbre graphique
         setContentView(R.layout.activity_creation_compte);
+
+        // Récupérer les vues
+        editTextNomView = findViewById(R.id.editTextNom);
+        editTextPrenomView = findViewById(R.id.editTextPrenom);
+        button_saveView = findViewById(R.id.button_save);
     }
 
     public void CreationCompteValider(View view) {
+
 
     }
 }
